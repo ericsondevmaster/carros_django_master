07369241a522f8736fe06b804fea01c1bd3b1a54
ericsondevmaster <ericson.devmaster@gmail.com>
2024-07-17 07:33:35 -0300

    Using CreateView

--- app/urls.py
+++ app/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from accounts.views import register_view, login_view, logout_view
-from cars.views import NewCarView, CarsListView
+from cars.views import CarsListView, NewCarCreateView
 
 
 urlpatterns = [
@@ -12,5 +12,5 @@ urlpatterns = [
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
     path('cars/', CarsListView.as_view(), name='cars_list'),
-    path('new_car/', NewCarView.as_view(), name='new_car'),
+    path('new_car/', NewCarCreateView.as_view(), name='new_car'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
