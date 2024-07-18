6b104a5145b94f427f910ff3b60ed9daa0ea6928
ericsondevmaster <ericson.devmaster@gmail.com>
2024-07-18 08:27:14 -0300

    Adjusting car_detail.html and Views authorization

--- cars/views.py
+++ cars/views.py
@@ -1,5 +1,7 @@
 from cars.models import Car
 from cars.forms import CarModelForm
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 
@@ -17,6 +19,7 @@ class CarsListView(ListView):
         return cars
 
 
+@method_decorator(login_required(login_url='login'), name='dispatch')
 class NewCarCreateView(CreateView):
     model = Car
     form_class = CarModelForm
@@ -29,6 +32,7 @@ class CarDetailView(DetailView):
     template_name = 'car_detail.html'
 
 
+@method_decorator(login_required(login_url='login'), name='dispatch')
 class CarUpdateView(UpdateView):
     model = Car
     form_class = CarModelForm
@@ -38,6 +42,7 @@ class CarUpdateView(UpdateView):
         return reverse_lazy('car_detail', kwargs={'pk': self.object.pk})
 
 
+@method_decorator(login_required(login_url='login'), name='dispatch')
 class CarDeleteView(DeleteView):
     model = Car
     template_name = 'car_delete.html'
